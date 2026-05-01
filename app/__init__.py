6f8914009cffc6e25493ff95470676d8aa29f3fb
Himank Gupta <himankgupta1605@gmail.com>
2026-05-01 16:01:12 +0530

    second upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,16 @@
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
 import pyotp
 
-# Use PyMySQL as the MySQLdb driver
-pymysql.install_as_MySQLdb()
-
-# Example TOTP secret (don’t hardcode in production)
+# TOTP
 totp = pyotp.TOTP("JBSWY3DPEHPK3PXP")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "sarvesh"
 
-# ✅ XAMPP default settings
-# - user: root
-# - password: "" (empty, unless you set one in phpMyAdmin)
-# - host: 127.0.0.1
-# - port: 3306
-# - database: dvwa   (replace with your DB name)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@127.0.0.1:3306/dvwa'
-
-# If you set a password for root (example: 'sarvesh'):
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:sarvesh@127.0.0.1:3306/dvwa'
+# ✅ PostgreSQL connection (Render)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://root:ORZJQlwukQOcReuYDEQaADoqCwBge4r1@dpg-d7q7uphkh4rs73b3935g-a/major_dbqa"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-# Optional: keep connections healthy
-# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}
